a954300531dada0a45cefe3f28db92e114058f3e
Vivek248-cloud <vivekofficial248.com>
2026-02-16 12:23:49 +0530

    Remove pycache files and add new files

--- eliteAcc/settings.py
+++ eliteAcc/settings.py
@@ -12,13 +12,13 @@ SECRET_KEY = 'django-insecure-ie926!pa)-1=%6zn*c#wz$=4y#wtv536a(^dxp58x5uj!vurcc
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+# DEBUG = True
 
-# DEBUG = False
+DEBUG = False
 
-ALLOWED_HOSTS = ['eliteaccounts.in', 'www.eliteaccounts.in', '31.97.62.126','*']
+# ALLOWED_HOSTS = ['eliteaccounts.in', 'www.eliteaccounts.in', '31.97.62.126','*']
 
-# ALLOWED_HOSTS = ['eliteaccounts.in', 'www.eliteaccounts.in', '31.97.62.126']
+ALLOWED_HOSTS = ['eliteaccounts.in', 'www.eliteaccounts.in', '31.97.62.126']
 
 # Application definition
 
@@ -78,29 +78,29 @@ WSGI_APPLICATION = 'eliteAcc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'eliteacc',
-        'USER': 'root',
-        'PASSWORD': 'Admin123',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
-
-
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'eliteaccounts_db',
-#         'USER': 'eliteaccountuser',
-#         'PASSWORD': 'Admin@123',
+#         'NAME': 'eliteacc',
+#         'USER': 'root',
+#         'PASSWORD': 'Admin123',
 #         'HOST': 'localhost',
 #         'PORT': '3306',
 #     }
 # }
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eliteaccounts_db',
+        'USER': 'eliteaccountuser',
+        'PASSWORD': 'Admin@123',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
 
 # Password validation
